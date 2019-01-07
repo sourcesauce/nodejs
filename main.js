872614d684e8741bb2916e57b632eb0affedbcf3
sourcesauce <sshlc940503@naver.com>
2019-01-08 05:17:26 +0900

    Not found 구현

--- main.js
+++ main.js
@@ -5,40 +5,40 @@ var url = require('url');
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
 
-  if (_url == '/') {
-    title = 'Welcome';
-  }
-  if (_url == '/favicon.ico') {
+  if (pathname === '/') {
+    fs.readFile(`data/${queryData.id}`, 'utf8',
+      function(err, description) {
+        var template = `
+      <!DOCTYPE html>
+      <html lang="en" dir="ltr">
+        <head>
+          <meta charset="utf-8">
+          <title>Web1 - ${title} Page</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">Web</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+      </html>
+      `;
+        response.writeHead(200);
+        response.end(template);
+      });
+  } else {
     response.writeHead(404);
+    response.end('Not found');
   }
 
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, 'utf8',
-  function(err,description){
-    var template = `
-    <!DOCTYPE html>
-    <html lang="en" dir="ltr">
-      <head>
-        <meta charset="utf-8">
-        <title>Web1 - ${title} Page</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">Web</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-    </html>
-    `;
-    response.end(template);
-  });
 
 });
 app.listen(3000);
