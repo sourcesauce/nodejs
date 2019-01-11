6fd6c80be42fadcdca9c2e8fc9ba8ae0651c959f
sourcesauce <sshlc940503@naver.com>
2019-01-12 04:28:50 +0900

    파일 추가 및 수정 ing

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
+
+function templateHTML(title, list, body, control) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title>Web1 - ${title} Page</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">Web</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+  </html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -15,70 +46,95 @@ var app = http.createServer(function(request, response) {
         var title = 'Welcome';
         var description = 'Hello, Node.js';
 
-        var list = '<ul>';
-        var i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i = i + 1;
-        }
-        list = list + '</ul>';
 
-        var template = `
-          <!DOCTYPE html>
-          <html lang="en" dir="ltr">
-            <head>
-              <meta charset="utf-8">
-              <title>Web1 - ${title} Page</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">Web</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-          </html>
-          `;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list,
+          `<h2>${title}</h2>${description}`,``);
+
         response.writeHead(200);
         response.end(template);
       })
     } else {
       fs.readdir('./data', function(error, filelist) {
 
-        var list = '<ul>';
-        var i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i = i + 1;
-        }
-        list = list + '</ul>';
-
         fs.readFile(`data/${queryData.id}`, 'utf8',
           function(err, description) {
             var title = queryData.id;
-            var template = `
-        <!DOCTYPE html>
-        <html lang="en" dir="ltr">
-          <head>
-            <meta charset="utf-8">
-            <title>Web1 - ${title} Page</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">Web</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-        </html>
-        `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            );
+
             response.writeHead(200);
             response.end(template);
           });
       })
     }
 
-  } else {
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function(error, filelist) {
+      var title = 'Web - create';
+
+
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+        <form action="/create_process" method="post">
+          <p><input type="text" name="title" placeholder="제목"></p>
+          <p>
+            <textarea name="description" placeholder="내용"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+
+      response.writeHead(200);
+      response.end(template);
+    })
+  } else if (pathname === '/create_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body = body + data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8',function(err){
+        response.writeHead(302,{Location:`/?id=${title}`});
+        response.end('Success');
+      })
+    });
+
+  } else if(pathname==='/update'){
+    fs.readdir('./data', function(error, filelist) {
+
+      fs.readFile(`data/${queryData.id}`, 'utf8',
+        function(err, description) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `<form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="제목" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="내용">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>`
+            ,``
+          );
+
+          response.writeHead(200);
+          response.end(template);
+        });
+    })
+  }else {
     response.writeHead(404);
     response.end('Not found');
   }
