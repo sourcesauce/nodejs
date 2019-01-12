bee212d2b86224213e24e6b26d34f1927589396f
sourcesauce <sshlc940503@naver.com>
2019-01-12 10:15:00 +0900

    삭제 기능 구현

--- main.js
+++ main.js
@@ -63,7 +63,12 @@ var app = http.createServer(function(request, response) {
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `<a href="/create">create</a>
+              <a href="/update?id=${title}">update</a>
+              <form action="delete_process" method="POST">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>`
             );
 
             response.writeHead(200);
@@ -155,11 +160,24 @@ var app = http.createServer(function(request, response) {
       });
 
     });
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function(error) {
+        response.writeHead(302, {
+          Location: `/`
+        });
+        response.end();
+      })
+    })
   } else {
     response.writeHead(404);
     response.end('Not found');
   }
-
-
 });
 app.listen(3000);
