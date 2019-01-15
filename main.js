1bb1a608ac83ba0b4b2a3efe657de7f3eb7c8a24
sourcesauce <sshlc940503@naver.com>
2019-01-16 00:03:26 +0900

    MySQL로 상세보기 구현, update,delete 버튼 구현

--- main.js
+++ main.js
@@ -29,39 +29,39 @@ var app = http.createServer(function(request, response) {
         var description = 'Hello, Node.js';
         var list = template.list(topics);
         var html = template.html(title, list,
-           `<h2>${title}</h2>${description}`,
-           `<a href="/create">create</a>`);
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`);
         response.writeHead(200);
         response.end(html);
-      })
+      });
     } else {
-      fs.readdir('./data', function(error, filelist) {
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8',
-          function(err, description) {
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-              allowedTags: ['h1']
-            });
 
-            var list = template.list(filelist);
-            var html = template.html(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-              `<a href="/create">create</a>
-              <a href="/update?id=${sanitizedTitle}">update</a>
+      connection.query(`SELECT * FROM topic`, function(error, topics) {
+        if (error) {
+          throw error;
+        }
+        connection.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic) {
+          if (error) {
+            throw error2;
+          }
+          var title = topic[0].title;
+          var description = topic[0].description;
+          var list = template.list(topics);
+          var html = template.html(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>
+              <a href="/update?id=${queryData.id}">update</a>
               <form action="delete_process" method="POST">
-                <input type="hidden" name="id" value="${sanitizedTitle}">
+                <input type="hidden" name="id" value="${queryData.id}">
                 <input type="submit" value="delete">
               </form>`
             );
+          response.writeHead(200);
+          response.end(html);
+        });
 
-            response.writeHead(200);
-            response.end(html);
-          });
-      })
+      });
     }
-
   } else if (pathname === '/create') {
     fs.readdir('./data', function(error, filelist) {
       var title = 'Web - create';
